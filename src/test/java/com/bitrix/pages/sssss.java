acd8177242929c4f82080b581d166e918d8e7a87
stefanmitkovic <stefanmitkovic@gmail.com>
2020-07-18 19:15:13 -0400

    Sss

---
+++ src/test/java/com/bitrix/pages/sssss.java
@@ -0,0 +1,10 @@
+package com.bitrix.pages;
+
+public class sssss {
+
+    public static void main(String[] args) {
+        System.out.println("Hey Nigina");
+        System.out.println("How are you? :)");
+
+    }
+}
